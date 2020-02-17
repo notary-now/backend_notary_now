4dbccca47d780e145d929215c30399a8be529695
Laura Schulz <lrs8810@gmail.com>
2020-02-17 16:10:47 -0700

    Add serializer for User model
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- backend_notary_now/settings.py
+++ backend_notary_now/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'notary_now_api',
-    'account'
+    'account',
+    'rest_framework'
 ]
 
 AUTH_USER_MODEL = 'account.Account'
--- notary_now_api/admin.py
+++ notary_now_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Location, Language, Notary, Appointment
+admin.site.register(Location)
+admin.site.register(Language)
+admin.site.register(Notary)
+admin.site.register(Appointment)
