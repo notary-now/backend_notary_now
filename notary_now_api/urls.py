a94255ee2af26a7236751670a904285bfcd4dedb
Laura Schulz <lrs8810@gmail.com>
2020-02-19 14:51:58 -0700

    Add endpoint to verify notary user
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- notary_now_api/urls.py
+++ notary_now_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import notary_users_list, notary_detail
+from .views import notary_users_list, notary_detail, notary_verify
 
 urlpatterns = [
     path("notaries/", notary_users_list, name="notary_users_list"),
-    path("notaries/<int:pk>/", notary_detail, name="notary_user_detail")
+    path("notaries/<int:pk>/", notary_detail, name="notary_user_detail"),
+    path("notaries/<int:pk>/verify", notary_verify, name="notary_user_verfiy")
 ]
