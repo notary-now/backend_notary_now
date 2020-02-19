209448d3639084e2a100e90475d8095b9abd086c
David Lopez <ldavid.x.lopezl@gmail.com>
2020-02-19 15:44:32 -0700

    Update typo of verify in urls
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: Laura Schulz <lrs8810@gmail.com>

--- notary_now_api/urls.py
+++ notary_now_api/urls.py
@@ -4,5 +4,5 @@ from .views import notary_users_list, notary_detail, notary_verify
 urlpatterns = [
     path("notaries/", notary_users_list, name="notary_users_list"),
     path("notaries/<int:pk>/", notary_detail, name="notary_user_detail"),
-    path("notaries/<int:pk>/verify", notary_verify, name="notary_user_verfiy")
+    path("notaries/<int:pk>/verify/", notary_verify, name="notary_user_verify")
 ]
