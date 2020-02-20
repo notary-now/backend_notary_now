a6b3f8c68c5cbcce86e5ccba1b616dd16dd77210
David Lopez <ldavid.x.lopezl@gmail.com>
2020-02-20 15:12:13 -0700

    Add view to add appointments
    Co-Authored-By: Laura Schulz <lrs8810@gmail.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- notary_now_api/views.py
+++ notary_now_api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse, HttpResponse
-from .models import User, Notary
+from .models import User, Notary, Appointment
 from .helpers.format_notary import format_notary
 import requests
 import json
+from django.views.decorators.csrf import csrf_exempt
 
 def notary_users_list(request):
     notaries = Notary.objects.select_related('user')
@@ -37,5 +38,31 @@ def notary_verify(request, pk):
             return JsonResponse({'success': 'Verified'}, status=200)
         else:
             return JsonResponse({'error': 'Unable to verify'}, status=404)
+    else:
+        return JsonResponse({'error': 'Notary not Found'}, status=404)
+
+@csrf_exempt
+def make_appointment(request, pk):
+    notary = Notary.objects.filter(user_id=pk)[0]
+    if notary:
+        appointment_info = json.loads(request.body)
+        appointment = Appointment.objects.filter(
+            notary_id=pk,
+            apointee_id=appointment_info['apointee_id'],
+            time=appointment_info['time'],
+            date=appointment_info['date'],
+            location=appointment_info['location'],
+        )
+        if not appointment:
+            Appointment.objects.create(
+                notary_id=pk,
+                apointee_id=appointment_info['apointee_id'],
+                time=appointment_info['time'],
+                date=appointment_info['date'],
+                location=appointment_info['location'],
+            )
+            return JsonResponse({'success': 'Appointment Created'}, status=201)
+        else:
+            return JsonResponse({'error': 'Not Unique'}, status=400)
     else:
         return JsonResponse({'error': 'Notary not Found'}, status=404)
