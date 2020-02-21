86e270789ac209ac04985a011969f4a9f40cd2af
Laura Schulz <lrs8810@gmail.com>
2020-02-20 17:24:02 -0700

    Add make appointment endpointment test
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>

--- notary_now_api/tests.py
+++ notary_now_api/tests.py
@@ -62,3 +62,33 @@ class OneNotaryTest(TestCase):
         self.assertEqual(json_response['notary_values']['verified'], self.notary.verified)
         self.assertEqual(json_response['notary_values']['bio'], self.notary.bio)
 
+class MakeAppointmentTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            first_name='David', last_name='Smith', email='jacob@turing.edu')
+        self.appointee = User.objects.create_user(
+            first_name='Karen', last_name='Smith', email='karen@turing.edu')
+        self.notary = Notary.objects.create(
+            state_notary_number='12345678',
+            commission_date='2020-02-10',
+            expiration_date='2022-02-10',
+            radius=7,
+            user_id=self.user.id
+        )
+
+    def test_appointments_endpoint(self):
+        request = self.factory.post(f'/api/v1/notaries/{self.user.id}/appointments',
+        data=json.dumps({
+            "appointee_id": self.appointee.id,
+            "date": "2020-02-28",
+            "time": "23:15:42",
+            "location": "Irving, TX, USA"
+         }),
+         content_type='application/json')
+        response = appointments(request, self.user.id)
+
+        self.assertEqual(response.status_code, 201)
+        json_response = json.loads(response.content)
+        self.assertEqual(json_response['success'], 'Appointment Created')
+
