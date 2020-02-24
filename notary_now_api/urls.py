4ca580b9e6bf83250d49627ce92c338ae1da7728
Brian Bower <brian@brianbower.com>
2020-02-24 10:06:13 -0700

    Add URL for individual appointments
    Co-Authored-By: Laura Schulz <lrs8810@gmail.com>
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>

--- notary_now_api/urls.py
+++ notary_now_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import notary_users_list, notary_detail, notary_verify, appointments
+from .views import notary_users_list, notary_detail, notary_verify, appointments, appointment_detail
 
 urlpatterns = [
     path("notaries/", notary_users_list, name="notary_users_list"),
     path("notaries/<int:pk>/", notary_detail, name="notary_user_detail"),
-    path("notaries/<int:pk>/appointments", appointments, name="notary_appointments"),
+    path("notaries/<int:notary_id>/appointments/<int:appointment_id>/", appointment_detail, name="notary_appointment_detail"),
+    path("notaries/<int:pk>/appointments/", appointments, name="notary_appointments"),
     path("notaries/<int:pk>/verify/", notary_verify, name="notary_user_verify")
 ]
