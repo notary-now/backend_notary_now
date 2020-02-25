ff811d20e70a8dd110a3ef57d4bad67e4745757a
David Lopez <ldavid.x.lopezl@gmail.com>
2020-02-25 13:57:02 -0700

    Add endpoints for logging in as a user and a notary user
    Co-Authored-By: Laura Schulz <lrs8810@gmail.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- notary_now_api/urls.py
+++ notary_now_api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import notary_users_list, notary_detail, notary_verify, appointments, appointment_detail
+from .views import notary_users_list, notary_detail, notary_verify, appointments, appointment_detail, notary_users_login, users_login
 
 urlpatterns = [
     path("notaries/", notary_users_list, name="notary_users_list"),
+    path("notaries/login/", notary_users_login, name="notary_users_login"),
+    path("users/login/", users_login, name="users_login"),
     path("notaries/<int:pk>/", notary_detail, name="notary_user_detail"),
     path("notaries/<int:notary_user_id>/appointments/<int:appointment_id>/", appointment_detail, name="notary_appointment_detail"),
     path("notaries/<int:pk>/appointments/", appointments, name="notary_appointments"),
