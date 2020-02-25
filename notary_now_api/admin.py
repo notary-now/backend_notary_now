8aaa504ebb1e48adf347e89f65265694be793466
David Lopez <ldavid.x.lopezl@gmail.com>
2020-02-25 13:54:41 -0700

    Update admin console to add filtering for passwords
    Co-Authored-By: Laura Schulz <lrs8810@gmail.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- notary_now_api/admin.py
+++ notary_now_api/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import Notary, Appointment, User
+from django import forms
+
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+class AccountAdmin(UserAdmin):
+    form = MyUserChangeForm
+    add_form = MyUserCreationForm
+    list_display = ('email', 'username', 'date_joined', 'last_login', 'is_admin','is_staff')
+    search_fields = ('email', 'first_name')
+    readonly_fields=('date_joined', 'last_login', 'username')
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
 admin.site.register(Notary)
 admin.site.register(Appointment)
-admin.site.register(User)
+admin.site.register(User, AccountAdmin)
